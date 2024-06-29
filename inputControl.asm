624b6ccc0c00f82a41cb9f6a395a66591b987114
BryanEstrada003 <mbestrad@espol.edu.ec>
2024-06-29 08:50:27 -0500

    Terminate input controls

--- inputControl.asm
+++ inputControl.asm
@@ -4,6 +4,8 @@
 msg db 0Dh, 0Ah, 'Ingrese una celda: $'   
 msg_correcto db 0Dh, 0Ah, 'Correcto$'   
 msg_incorrecto db 0Dh, 0Ah, 'Incorrecto, intente de nuevo$' 
+fila db ?
+columna db ?
 
 num dw 0
 
@@ -16,18 +18,35 @@ main proc
 ingreso:
     lea dx, msg
     mov ah, 09h
-    int 21h
+    int 21h 
     
     ; Leer el primer dígito del número ingresado por el usuario
     mov ah, 01h
-    int 21h     
+    int 21h
+    mov fila, al
     
-    cmp al, 41h
+    ; Leer el segundo digito
+    mov ah, 01h
+    int 21h
+    mov columna, al
+    
+    ;verificar si se ingresaron dos caracteres
+    cmp fila, 0h
+    je incorrecto
+    cmp columna, 0
+    je incorrecto     
+    
+    cmp fila, 41h
     jb incorrecto
-    cmp al, 47h
+    cmp fila, 47h
+    ja incorrecto 
+    
+    cmp columna, 31h
+    jb incorrecto
+    cmp columna, 36h
     ja incorrecto
     
-    ; Mostrar mensaje de correcto
+    ; Mostrar mensaje de correcto 
     lea dx, msg_correcto
     mov ah, 09h
     int 21h
@@ -46,10 +65,3 @@ fin:
 
 
 end main
-
-
-
-
-
-
-
